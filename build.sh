34feaa301593d42d60cb2e885103e465376334d4
ArnaudAstree <arnaud.zdziobeck@astree>
2016-02-26 18:23:22 +0100

    Modification conf pour dev env

--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ mvn clean install
 docker build -t laloutre42/docker-foodrecipes-server .
 
 # Run foodrecipes-server
-docker stop foodrecipes-server
-docker rm foodrecipes-server
-docker run --name foodrecipes-server -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=aws --link foodrecipes-mongo:mongo laloutre42/docker-foodrecipes-server
+docker stop foodrecipes-server && docker rm foodrecipes-server && docker run --name foodrecipes-server -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=aws --link foodrecipes-mongo:mongo laloutre42/docker-foodrecipes-server
