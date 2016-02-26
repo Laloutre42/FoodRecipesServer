33e410944a71a02fc6c7e821fb86c6946ab99397
Arnaud Zdziobeck <Laloutre42@gmail.com>
2016-02-26 21:05:56 +0100

    Maj build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@
 # Build foodrecipes-server
 mvn clean install
 docker build -t laloutre42/docker-foodrecipes-server .
+docker login --username=laloutre42 --password=Maximus42 --email=Laloutre42@gmail.com
+docker push laloutre42/docker-foodrecipes-server
 
 # Run foodrecipes-server
-docker stop foodrecipes-server
-docker rm foodrecipes-server
-docker run --name foodrecipes-server -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=aws --link foodrecipes-mongo:mongo laloutre42/docker-foodrecipes-server
+#docker stop foodrecipes-server
+#docker rm foodrecipes-server
+#docker run --name foodrecipes-server -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=aws --link foodrecipes-mongo:mongo laloutre42/docker-foodrecipes-server
